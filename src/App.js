b44bd5f8d52424cefd9830e9b2c0fc6070527854
Younous Saleh <younisssaleh10@gmail.com>
2022-10-12 09:23:47 -0400

    search and delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import NotesList from "./components/NodesList";
 import { nanoid } from "nanoid";
+import Search from "./components/Search";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -48,6 +49,7 @@ const App = () => {
 
   return (
     <div className="container">
+      <Search />
       <NotesList
         notes={notes}
         handleAddNote={addNote}
